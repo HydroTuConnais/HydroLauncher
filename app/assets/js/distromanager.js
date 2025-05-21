0a5611849cd33b214765d3f84f85e2c90b02fef9
HydroTuConnais <hydrotuconnais@gmail.com>
2025-05-22 00:34:05 +0200

    distrib

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/HydroTuConnais/HydroLauncher/64989042abd99ca15384451fea96abbd2ccb7731/docs/sample_distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/HydroTuConnais/HydroLauncher/master/docs/sample_distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
